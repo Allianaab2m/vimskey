abe428b8b0b9091081c6a348a0d01b257ec6d38a
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-11-12 20:28:25 +0900

    Zod追加

--- denops/vimskey/deps.ts
+++ denops/vimskey/deps.ts
@@ -4,3 +4,4 @@ export * as helper from "https://deno.land/x/denops_std@v3.9.1/helper/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 export * as Misskey from "npm:misskey-js@0.0.14";
 // export * as Misskey from "https://esm.sh/misskey-js@0.0.14"; // 開発時には補完を有効にするためこっちをインポートする
+export { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
