082abab9c648688623aeaebc478e90cf7be3bf95
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-11-17 22:09:40 +0900

    Deps update

--- denops/vimskey/deps.ts
+++ denops/vimskey/deps.ts
@@ -2,7 +2,8 @@ export type { Denops } from "https://deno.land/x/denops_std@v3.9.1/mod.ts";
 export * as buffer from "https://deno.land/x/denops_std@v3.9.1/buffer/mod.ts";
 export * as helper from "https://deno.land/x/denops_std@v3.9.1/helper/mod.ts";
 export * as variable from "https://deno.land/x/denops_std@v3.9.1/variable/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.9.1/mapping/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.9.1/function/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 export * as Misskey from "npm:misskey-js@0.0.14";
-// export * as Misskey from "https://esm.sh/misskey-js@0.0.14"; // 開発時には補完を有効にするためこっちをインポートする
 export { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
